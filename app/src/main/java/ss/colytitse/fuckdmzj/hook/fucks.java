7b7b1c83ca7d0a46b69f14eb08093cae4ad7b90e
cokkeijigen <67277503+cokkeijigen@users.noreply.github.com>
2021-10-25 15:22:05 +0800

    Update fucks.java

--- app/src/main/java/ss/colytitse/fuckdmzj/hook/fucks.java
+++ app/src/main/java/ss/colytitse/fuckdmzj/hook/fucks.java
@@ -1,12 +1,24 @@
 package ss.colytitse.fuckdmzj.hook;
 
 import android.app.Activity;
+import android.view.View;
+import android.widget.RelativeLayout;
+import java.lang.reflect.Field;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
+import ss.colytitse.fuckdmzj.MainHook;
 
 public class fucks {
 
-    // 关闭广告
+    // 获取控件
+    private static Object getView(XC_MethodHook.MethodHookParam param,String id_name) throws Throwable {
+        Class clazz = param.thisObject.getClass();
+        Field field = clazz.getDeclaredField(id_name);
+        field.setAccessible(true);
+        return  (Object) field.get(param.thisObject);
+    }
+
+    // 去除广告
     public static void fuck_AD(ClassLoader classLoader, String PKGN){
         
         {   // 规则一
@@ -31,6 +43,23 @@ public class fucks {
                         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                             XposedHelpers.callMethod(param.thisObject, "onAdCloseView");
                             param.setResult(null);
+                        }
+                    }
+            );
+        }
+
+        {   // 去除详细页的广告位
+            String ActVeCla = (PKGN.equals(MainHook.DMZJ_PKGN)) ?
+                    ".ui.CartoonInstructionActivity":
+                    ".ui.NovelInstructionActivity";
+            XposedHelpers.findAndHookMethod(
+                    XposedHelpers.findClass(PKGN + ActVeCla, classLoader),
+                    "findViews",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            RelativeLayout rl = (RelativeLayout) getView(param,"layout_ad_layout");
+                            rl.setVisibility(View.GONE);
                         }
                     }
             );
